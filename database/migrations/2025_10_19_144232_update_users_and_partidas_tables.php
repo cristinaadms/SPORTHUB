5b4b8e53584e96edef1c6ec55d9574eceef42b1d
Malujoro <mateus_rocha_sousa@hotmail.com>
2025-10-19 12:20:57 -0300

    Ajuste nas migrations (criação de campo nome e adição de valores nos enums)

--- database/migrations/2025_10_19_144232_update_users_and_partidas_tables.php
+++ database/migrations/2025_10_19_144232_update_users_and_partidas_tables.php
@@ -7,27 +7,26 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        // Remover coluna 'senha' da tabela users
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('senha');
-        });
-
         // Adicionar coluna 'descricao' na tabela partidas
         Schema::table('partidas', function (Blueprint $table) {
             $table->text('descricao')->nullable()->after('nome'); // "after" é opcional
+        });
+
+        Schema::table('partidas', function (Blueprint $table) {
+            $table->text('nome')->after('id'); // "after" é opcional
         });
     }
 
     public function down(): void
     {
-        // Restaurar coluna 'password' em users
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('senha');
+        // Remover coluna 'descricao' de partidas
+        Schema::table('partidas', function (Blueprint $table) {
+            $table->dropColumn('descricao');
         });
 
         // Remover coluna 'descricao' de partidas
         Schema::table('partidas', function (Blueprint $table) {
-            $table->dropColumn('descricao');
+            $table->dropColumn('nome');
         });
     }
 };
